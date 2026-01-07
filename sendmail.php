9da3106dca762232fa7cd8244d8cc2f56b8a44c6
wahyuyassin <wahyuyassin@gmail.com>
2026-01-07 16:10:11 +0700

    Add contact form and email functionality to team and index pages

---
+++ sendmail.php
@@ -0,0 +1,37 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '/usr/share/php/libphp-phpmailer/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+
+    $mail->isSMTP();
+    $mail->Host       = 'mail.trustwe.my.id';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'admin@trustwe.my.id';
+    $mail->Password   = 'PASSWORD_EMAIL_KAMU';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+    $mail->setFrom('admin@trustwe.my.id', 'TrustWe Website');
+    $mail->addAddress('admin@trustwe.my.id');
+
+    $mail->Subject = "Pesan dari Website TrustWe";
+
+    $body  = "Nama   : " . $_POST['Nama'] . "\n";
+    $body .= "Subjek : " . $_POST['Subjek'] . "\n\n";
+    $body .= "Pesan  : \n" . $_POST['Pesan'];
+
+    $mail->Body = $body;
+
+    $mail->send();
+
+    echo "Pesan berhasil dikirim 🙂";
+
+} catch (Exception $e) {
+    echo "Gagal mengirim email. Error: {$mail->ErrorInfo}";
+}
+?>
